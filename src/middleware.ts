c677102d93422005de8a624e501857c81fccf48e
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-06-28 09:31:09 +0545

    Fix auth path

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,7 +43,7 @@ export default auth(async (req) => {
     }
 
     if (!isLoggedIn && !isPublicRoute) {
-        return NextResponse.redirect(new URL("/auth/login", nextUrl));
+        return NextResponse.redirect(new URL("/auth", nextUrl));
     }
 
     return;
