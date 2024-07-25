811fe82759bdfe347f737a573523102b37a5101e
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-07-25 08:14:52 +0545

    Added demo explanation

--- src/components/demo/demo.tsx
+++ src/components/demo/demo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { DATA } from "@/data"
 import { cn } from '@/lib/utils'
 import RequestDemo from './request-demo'
-import { TypographyH3 } from '../ui/typography'
+import { TypographyH3, TypographyP } from '../ui/typography'
 import FeedbackTableDemo from './feedback-table-demo'
 
 const Demo = () => {
@@ -12,7 +12,8 @@ const Demo = () => {
 
   return (
     <section className={cn("my-8 space-y-8 flex flex-col justify-center items-center")}>
-      <TypographyH3 className='text-center'>Explore API Endpoints</TypographyH3>
+      <TypographyH3 className='text-center -mb-4'>Explore API Endpoint</TypographyH3>
+      <TypographyP className='hidden md:flex text-center opacity-70'>Send feedback with required data and see the table get updated</TypographyP>
 
       <div className='w-full flex flex-col lg:flex-row gap-4 justify-between items-start'>
         <div className={cn('w-full lg:w-auto')}>
